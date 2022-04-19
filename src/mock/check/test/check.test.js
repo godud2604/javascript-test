2781dc910b9622700ee29ea84b9db739535d1315
godud2604 <ees238@naver.com>
2022-04-19 14:46:28 +0900

    Add mock test 주석

--- src/mock/check/test/check.test.js
+++ src/mock/check/test/check.test.js
@@ -4,18 +4,21 @@ describe('check', () => {
   let onSuccess;
   let onFail;
 
+  // 초기화
   beforeEach(() => {
     onSuccess = jest.fn(); // mock function 
-    onFail = jest.fn();
+    onFail = jest.fn(); // // mock function 
   });
 
   it('should call onSuccess when predicate is true', () => {
     check(() => true, onSuccess, onFail);
 
     // expect(onSuccess.mock.calls.length).toBe(1);
+    // toHaveBeenCalledTimes : 호출된 횟수
     expect(onSuccess).toHaveBeenCalledTimes(1);
     
     // expect(onSuccess.mock.calls[0][0]).toBe('yes');
+    // toHaveBeenCalledWith : 이 인자와 함께 호출되어야 한다.
     expect(onSuccess).toHaveBeenCalledWith('yes');
 
     // expect(onFail.mock.calls.length).toBe(0);
